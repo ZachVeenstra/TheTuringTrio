5aa9ba48330af6f4387fb2ac20c475374a6bfcde
Zach Veenstra <zach.veen.gr@gmail.com>
2023-11-15 08:46:55 -0500

    Added base file for EditProfileView.

---
+++ src/SmartJournal/SmartJournal/Features/Profile/EditProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  EditProfileView.swift
+//  SmartJournal
+//
+//  Created by Zach Veenstra on 11/15/23.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditProfileView()
+}
