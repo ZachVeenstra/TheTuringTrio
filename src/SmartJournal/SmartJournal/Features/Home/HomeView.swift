a31d46960f82b22056bef19ae757b5b8619df714
usmanq <qureshiu@mail.gvsu.edu>
2023-12-09 17:47:49 -0500

    Images shown on the homeview

--- src/SmartJournal/SmartJournal/Features/Home/HomeView.swift
+++ src/SmartJournal/SmartJournal/Features/Home/HomeView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 import FirebaseAuth
 import FirebaseFirestore
+import FirebaseStorage
+import Kingfisher
 
 struct Journal: Identifiable, Codable {
     @DocumentID var id: String?
@@ -19,17 +21,26 @@ struct Journal: Identifiable, Codable {
 
 struct HomeView: View {
     @State private var journals: [Journal] = []
-
+    @State private var downloadedImages: [String: [UIImage]] = [:]
+    
     var body: some View {
         NavigationView {
             List(journals) { journal in
                 VStack(alignment: .leading) {
+                    let entryImages = downloadedImages[journal.id ?? ""]
+                    ImageCarousel(photos: downloadedImages[journal.id ?? ""] ?? [])
+                    .frame(height: 200)
+                    .onAppear {
+                        downloadImages(for: journal)
+                    }
+
                     Text(journal.title)
                         .font(.headline)
                     Text(journal.body)
                         .foregroundColor(.gray)
+                    
                 }
-                .padding()
+                //.padding()
             }
             .onAppear {
                 fetchJournals()
@@ -83,6 +94,49 @@ struct HomeView: View {
             }
         }
     }
+    private func loadImages(from photoURLs: [String]) -> [UIImage] {
+        var images: [UIImage] = []
+
+        for photoURL in photoURLs {
+            // Assuming "JournalImages" is the folder in Firebase Storage
+            let storageReference = Storage.storage().reference().child("JournalImages/").child(photoURL)
+
+            storageReference.getData(maxSize: Int64.max) { data, error in
+                if let error = error {
+                    print("Error fetching image data for \(photoURL): \(error.localizedDescription)")
+                } else if let data = data, let image = UIImage(data: data) {
+                    images.append(image)
+                }
+            }
+        }
+
+        return images
+    }
+    private func downloadImages(for journal: Journal) {
+        guard let journalID = journal.id else { return }
+            for photoURL in journal.photos {
+                let storageRef = Storage.storage().reference(forURL: photoURL)
+
+                storageRef.getData(maxSize: 5 * 1024 * 1024) { data, error in
+                    if let error = error {
+                        print("Error downloading image: \(error)")
+                    } else {
+                        if let data = data, let image = UIImage(data: data) {
+                            // Update the downloaded images for the specific journal entry
+                            DispatchQueue.main.async {
+                                if var entryImages = downloadedImages[journalID] {
+                                    entryImages.append(image)
+                                    downloadedImages[journalID] = entryImages
+                                } else {
+                                    downloadedImages[journalID] = [image]
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
 }
 
 
